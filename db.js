db9bc636eb3505058b4c1a04215919359432cec2
Vilsium <vilsium28@gmail.com>
2024-11-28 02:15:33 +0530

    Backend ready and working tested with postman

--- db.js
+++ db.js
@@ -1,1 +1,34 @@
 //SQLite databse connection
+
+//importing the required modules
+const sqlite3 = require("sqlite3").verbose();
+const dbPath = process.env.DB_PATH || "./tasks.db";
+
+//making the schema
+const db = new sqlite3.Database(dbPath, (err) => {
+  if (err) {
+    console.error("Error opening database: ", err.message);
+  } else {
+    console.log("Connected to SQLite Database!");
+    db.run(
+      `
+        CREATE TABLE IF NOT EXISTS Task (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL,
+            description TEXT,
+            due_date TEXT,
+            status TEXT DEFAULT 'Pending'
+        )
+            `,
+      (err) => {
+        if (err) {
+          console.error("Error creating table: ", err.message);
+        } else {
+          console.log("Task table is ready!");
+        }
+      }
+    );
+  }
+});
+
+module.exports = db;
--- server.js
+++ server.js
@@ -1,1 +1,91 @@
 //backend server
+
+//importing required modules
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const db = require("./db");
+
+//initializing express
+const app = express();
+
+//MiddlewAare
+app.use(cors());
+app.use(bodyParser.json());
+
+//Creating APIs End-Points (Routes)
+//creating a new task
+app.post("/tasks", (req, res) => {
+  //   console.log(req.body);
+  const { title, description, due_date } = req.body;
+  const status = req.body.status || "Pending"; //settinf default status as pendinf if not provided
+
+    if (!title || !description || !due_date) {
+        return res.status(400).json({ error: "All fields are required!" });
+    }
+
+  db.run(
+    `INSERT INTO Task (title, description, due_date, status) VALUES (?, ?, ?, ?)`,
+    [title, description, due_date, status],
+    function (err) {
+      if (err) {
+        res.status(400).json({ error: err.message });
+      } else {
+        console.log(req.body);
+        res.status(200).json({ id: this.lastId });
+      }
+    }
+  );
+});
+
+//getting all tasks
+app.get("/tasks", (req, res) => {
+  db.all(`SELECT * FROM Task`, [], (err, rows) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+    } else {
+      res.json(rows);
+    }
+  });
+});
+
+//updating a task
+app.put("/tasks/:id", (req, res) => {
+  const { id } = req.params;
+  const { title, description, due_date, status } = req.body;
+
+  if (!title || !description || !due_date) {
+    return res.status(400).json({ error: "All fields are required!" });
+  }
+
+  db.run(
+    `UPDATE Task SET title = ?, description = ?, due_date = ?, status = ?, WHERE id = ?`,
+    [title, description, due_date, status, id],
+    function (err) {
+      if (err) {
+        res.status(400).json({ error: err.message });
+      } else {
+        res.json({ updatedRows: this.changes });
+      }
+    }
+  );
+});
+
+//deleting a task
+app.delete("/tasks/:id", (req, res) => {
+  const { id } = req.params;
+  db.run(`DELETE FROM Task WHERE id = ?`, [id], function (err) {
+    if (err) {
+      res.status(400).json({ error: err.message });
+    } else {
+      res.json({ deletedRows: this.changes });
+    }
+  });
+});
+
+//starting the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
